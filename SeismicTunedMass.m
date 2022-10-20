09736dcf51eeddb7521255595261985595ca766c
Aman Gautam <13annon@gmail.com>
2022-10-21 00:03:17 +0530

    Adjusted params

--- SeismicTunedMass.m
+++ SeismicTunedMass.m
@@ -2,15 +2,15 @@ clc
 close all
 clear all
 
-xg = .0;
+xg = .01;
 w1 = 1;
 we = 1;
-eta = .5;
+eta = 0.2;
 w2 = 1;
 
 dt = .05;
 t_span = 0:dt:dt*1000;
-x0 = [0 1 0 0]; %x1 , x1dot , x2 , x2dot 
+x0 = [0 0 0 0]; %x1 , x1dot , x2 , x2dot 
 f = @(t,x)[x(2);
     w2^2*eta*x(3) - (w2^2*eta + w1^2)*x(1) + we*we*xg*sin(we*t);
     x(4);
