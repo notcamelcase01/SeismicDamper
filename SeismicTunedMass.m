2669fd105603f4014b20a31d3099a8020fa50ccf
Aman Gautam <13annon@gmail.com>
2022-10-21 08:45:33 +0530

    add comparision

--- SeismicTunedMass.m
+++ SeismicTunedMass.m
@@ -17,8 +17,19 @@ f = @(t,x)[x(2);
     we*we*xg*sin(we*t)-w2^2*(x(3)-x(1))];
 [t,x] = ode45(f,t_span,x0);
 plot(t_span,x(:,1));
-
-
+hold on
+xg = .01;
+w1 = 1;
+we = 1;
+eta = 0.0;
+w2 = 0;
+f1 = @(t,x)[x(2);
+    w2^2*eta*x(3) - (w2^2*eta + w1^2)*x(1) + we*we*xg*sin(we*t);
+    x(4);
+    we*we*xg*sin(we*t)-w2^2*(x(3)-x(1))];
+[t,x] = ode45(f1,t_span,x0);
+plot(t_span,x(:,1));
+hold off
 
 
 
